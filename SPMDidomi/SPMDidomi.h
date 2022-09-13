18f2331363ee54bced0c6f117a912201ac247cc8
Léo Marcotte <leo.marcotte@radiofrance.com>
2022-09-13 12:03:34 +0200

    First commit

---
+++ SPMDidomi/SPMDidomi.h
@@ -0,0 +1,18 @@
+//
+//  SPMDidomi.h
+//  SPMDidomi
+//
+//  Created by Leo Marcotte on 13/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMDidomi.
+FOUNDATION_EXPORT double SPMDidomiVersionNumber;
+
+//! Project version string for SPMDidomi.
+FOUNDATION_EXPORT const unsigned char SPMDidomiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMDidomi/PublicHeader.h>
+
+
